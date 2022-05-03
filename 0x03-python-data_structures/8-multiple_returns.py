75afb9aaa7a2b214ece4a74cbba638b7e99fe302
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-02 19:25:20 -0700

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@ def multiple_returns(sentence):
         _length = len(sentence)
         _first = sentence[0]
     else:
-        _first[0] = None
+        sentence[0] = None
         return (0, _first)
     return _length, _first

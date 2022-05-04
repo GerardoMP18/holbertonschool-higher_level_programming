cafcb78c384eef2e34de3daa5a1fff941660c2d9
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-04 12:55:47 -0700

    Adding write search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_my_list = my_list.copy()
     for i in range(len(my_list)):
-        if new_my_list[i] == 2:
+        if new_my_list[i] == search:
             new_my_list[i] = replace
     return new_my_list

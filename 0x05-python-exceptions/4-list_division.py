553134b6fc58b977a59bed9476e57e9bbeb2c7e2
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-16 11:46:06 -0700

    Delete variable zero

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    zero = 0
     for i in range(list_length):
         try:
             new_list.append(my_list_1[i] / my_list_2[i])

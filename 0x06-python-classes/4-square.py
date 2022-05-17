ea71068041f73f31eeaf8deac154d37aea58f618
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-17 15:28:18 -0700

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Access and update private attribute"""
+
+
+class Square:
+    """Creating of instantiation with size
+       private instance attibute size
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if(type(self.__size) is not int):
+            raise TypeError("size must be an integer")
+        if(self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")

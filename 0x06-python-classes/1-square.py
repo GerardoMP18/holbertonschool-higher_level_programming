d8fa5719a96404686be6fb66e5ee0438095beb7f
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-17 11:47:25 -0700

    Creating of instantiation with size and attribute size in private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Creating of instantiation with size
+       private instance attibute size
+    """
+    def __init__(self, size=0):
+        self.__size = size

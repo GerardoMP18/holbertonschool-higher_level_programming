6fb68c2d89fad6551501f9363365ce688d5eb45e
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-17 14:14:01 -0700

    method area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Area of a square"""
+
+
+class Square:
+    """Creating of instantiation with size
+       private instance attibute size
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if(type(self.__size) is not int):
+            raise TypeError("size must be an integer")
+        if(self.__size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size

ac96d714552c418f54a3841e57c2b4d411d7525d
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-23 13:18:53 -0700

    Initialization of attributes and creation of getters and setters

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Real definition of a rectangle"""
+
+
+class Rectangle:
+    """Creating of instantiation with width
+        private instance attribute height
+    """
+    def __init__(self, width=0, height=0):
+        """Initialization of rectagule"""
+        self.__width = width
+        self.__height = height
+        if (type(self.__width) is not int):
+            raise TypeError("width must be an integer")
+        elif(self.__width < 0):
+            raise ValueError("width must be >= 0")
+        elif (type(self.__height) is not int):
+            raise TypeError("height must be an integer")
+        elif (self.__height < 0):
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        elif(value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value

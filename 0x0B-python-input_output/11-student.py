b330428eb6ac17d12dcf72e8991e445d1dc06166
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-31 18:29:53 -0700

    reload from json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Student to JSON
+"""
+
+
+class Student:
+    """
+    Class creation to return the representation of the dictionary
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if(type(attrs) is list):
+            new_list = {}
+            for i in attrs:
+                try:
+                    new_list[i] = self.__dict__[i]
+                except Exception:
+                    pass
+        else:
+            new_list = self.__dict__
+
+        return new_list
+
+    def reload_from_json(self, json):
+        for x in json:
+            self.__dict__[x] = json[x]

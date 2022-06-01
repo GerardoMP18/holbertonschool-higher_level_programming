fbc1124d0dccb9fb873b7358679feb7ab56c9c45
gerardomp18 <gerardomarinparra18@gmail.com>
2022-05-31 17:18:13 -0700

    Class creation to return the representation of the dictionary

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Student to JSON
+"""
+
+
+class Student:
+    """
+    Class creation to return the representation of the dictionary
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__

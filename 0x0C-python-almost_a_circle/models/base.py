17d6f195e2b519457cb650e6f861e9e30bd5e45a
gerardomp18 <gerardomarinparra18@gmail.com>
2022-06-06 23:19:53 -0500

    Adding task Advanced

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -85,3 +85,34 @@ class Base:
                 return new
         except Exception:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        JSON string representation of list_objs in a file
+        """
+        new_list = []
+        if list_objs:
+            for i in list_objs:
+                dictionary = i.to_dictionary()
+                new_list.append(dictionary)
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode="w", encoding="utf-8")as myFile:
+            myFile.write(cls.to_json_string(new_list))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        returns a list of instances
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, mode="r", encoding="utf-8") as myFile:
+                readFile = myFile.read()
+                list_dictionary = cls.from_json_string(readFile)
+                new = []
+                for items in list_dictionary:
+                    new.append(cls.create(**items))
+                return new
+        except Exception:
+            return []

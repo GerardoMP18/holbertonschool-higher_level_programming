003384a5403823229d0a52af21329c0d96b1bc40
gerardomp18 <gerardomarinparra18@gmail.com>
2022-07-05 14:36:33 -0700

    Script that converrts hbtn_0c_0 database to UFT8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,11 @@
 -- Script that converrts hbtn_0c_0 database to UFT8
 ALTER DATABASE hbtn_0c_0
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
-USE hbtn_0c_0;
-ALTER TABLE first_table
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
 	CONVERT TO CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 	CHANGE name name VARCHAR(256)
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;

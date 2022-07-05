dee0d26f137b032b2c1360902a576b0935c3b1bf
gerardomp18 <gerardomarinparra18@gmail.com>
2022-07-05 14:40:34 -0700

    Adding task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,6 +6,7 @@ ALTER TABLE hbtn_0c_0.first_table
 	CONVERT TO CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
-	CHANGE name name VARCHAR(256)
+	CHANGE name name
+	VARCHAR(256)
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;

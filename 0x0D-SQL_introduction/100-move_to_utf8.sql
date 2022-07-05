af1b7f4b9865c82b8d498ef4ae1efd48373cf406
gerardomp18 <gerardomarinparra18@gmail.com>
2022-07-05 15:07:29 -0700

    Adding taks 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,12 @@
 -- Script that converrts hbtn_0c_0 database to UFT8
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table
+ALTER TABLE first_table
 	CONVERT TO CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table
-	CHANGE name name
-	VARCHAR(256)
+ALTER TABLE first_table
+	CHANGE name name VARCHAR(256)
 	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;

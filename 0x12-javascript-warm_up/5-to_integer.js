9e732def96335edde83f8af9ec60c44849522c88
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 13:51:18 -0500

    Script that converted in integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that converted to an integer
+
+const myArgv = process.argv[2];
+
+if (isNaN(myArgv)) {
+  console.log('Not a number');
+}
+else {
+  const convertInteger = Math.floor(myArgv);
+  console.log(`My number: ${convertInteger}`);
+}

360f792711a3c67c1b3db71cbd58728b8d0496df
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 14:03:26 -0500

    Adding script that print 3 lines with while

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that print 3 lines with while
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+while (i < languages.length) {
+  console.log(languages[i])
+  i++;
+}

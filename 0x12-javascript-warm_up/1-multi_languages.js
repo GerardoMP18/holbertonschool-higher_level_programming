caded61b69668b17dab9b62b4a68dfcfa790c930
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 12:21:58 -0500

    Adding exercises allows printing 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that prints 3 lines
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+	console.log(languages[i]);
+}

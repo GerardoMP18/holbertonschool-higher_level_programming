e6211d60a7286e427093da3342b1263cf0d72d91
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 17:29:50 -0500

    adding function factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Script that computes and prints a factorial
+
+function factorial (n) {
+  if (isNaN(n) || n <= 0) {
+	return (1);
+  } else {
+    return (parseInt(n) * factorial(n - 1));
+  }
+}
+
+const myArgv = process.argv[2];
+
+console.log(factorial(parseInt(myArgv)));

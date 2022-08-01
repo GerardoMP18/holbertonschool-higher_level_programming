dc516dfc8409367c98d7fa560c89b0cd27d561c1
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 13:13:16 -0500

    Script that prints the first arguments passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script that prints the fist arguments passed to it
+
+const myArgv = process.argv[2];
+
+if (myArgv === undefined) {
+  console.log('No argument');
+}
+else {
+  console.log(myArgv);
+}

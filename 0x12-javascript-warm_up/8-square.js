229b2e9e03ad3a9fd9a7d177825cf8ee714c8d60
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 15:06:05 -0500

    Adding script that prints s square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script that prints a square
+
+const myArgv = process.argv[2];
+
+if (isNaN(myArgv)) {
+  console.log('Missing size')
+}
+else {
+  const convertInteger = Math.floor(myArgv);
+ 
+  for (let i = 0; i < convertInteger; i++) {
+    console.log('X'.repeat(convertInteger));
+  }
+}

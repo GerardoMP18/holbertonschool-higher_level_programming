9beb186aa5bb5d3f8ccb9e49f2607eaac409a6e3
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 15:05:37 -0500

    Addin script that print x times c is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script that print x times  C is fun
+
+const myArgv = process.argv[2];
+
+if (isNaN(myArgv)) {
+  console.log('Missing number of occurrences');
+}
+else {
+  const convertInteger = Math.floor(myArgv);
+  
+  for (let i = 0; i < convertInteger; i++) {
+    console.log('C is fun');
+  }  
+}

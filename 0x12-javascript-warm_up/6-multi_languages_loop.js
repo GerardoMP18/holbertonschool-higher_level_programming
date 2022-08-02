2385c2467d27e4f60b53d3ab5ee95f95429fd70b
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-02 00:11:09 -0500

    Correction of requeriments

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -5,6 +5,6 @@ const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 let i = 0;
 while (i < languages.length) {
-  console.log(languages[i])
+  console.log(languages[i]);
   i++;
 }

576d2320536293604fc48ec4cd898ecb52c50b47
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-02 00:56:09 -0500

    Correction of space

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,7 +7,7 @@ if (isNaN(myArgv)) {
   console.log('Missing number of occurrences');
 } else {
   const convertInteger = Math.floor(myArgv);
-  
+
   for (let x = 0; x < convertInteger; x++) {
     console.log('C is fun');
   }

40dd1b3195abadfeb6d599d27ed1c10db2d093bf
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-02 01:06:32 -0500

    Correction of cheker

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// function that return the addition of 2 integers
+// function that return the addition of 2 integers aaa
 
 exports.add = function (a, b) {
   return (a + b);
-}
+};

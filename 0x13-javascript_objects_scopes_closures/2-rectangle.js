4aab828e2fb2256cebff8449f15cc6e4e5f2a2d9
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-02 01:16:01 -0500

    Adding conditional in constructor

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,8 +5,7 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
       return new Rectangle();
-    }
-    else if (w >= 1 && h >= 1) {
+    } else if (w >= 1 && h >= 1) {
       this.width = w;
       this.height = h;
     }

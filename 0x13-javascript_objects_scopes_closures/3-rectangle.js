025c64fc42cb79dbf3316bc3e75838e56106674a
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 22:56:44 -0500

    Adding method called print() that prints the rectangles using the character x

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Create an instance method called print() that prints the rectangle using the character X
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return new Rectangle();
+	} else if (w >= 1 && h >= 1) {
+      this.width = w;
+	  this.height = h;
+	}
+  }
+
+  //creation of method
+  print () {
+	const character = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
+	}
+  }
+};

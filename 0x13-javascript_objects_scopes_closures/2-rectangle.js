72f01a080ea931567932b6c755012d666b901f2f
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-01 22:44:11 -0500

    Addinc conditional in constructor

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// If w or h is equal to 0 or not a positive integer, create an empty object
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+	if (w <= 0 || h <= 0) {
+	  return new Rectangle();
+	}
+	else if (w >= 1 && h >= 1) {
+      this.width = w;
+	  this.height = h;
+	}
+  }
+};

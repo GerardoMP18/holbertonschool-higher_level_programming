e5ab335aff3c2b6edf25c748ff8e5750c56e1627
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-02 01:36:10 -0500

    correction of exercises 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 // Creation of new methods charPrint that prints the recatngles using the character c o x ....
 
-const newSquare = require('./5-square');
+const Rectangle = require('./4-rectangle.js');
 
-module.exports = class Square extends newSquare {
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
     if (c === undefined) {
       this.print();

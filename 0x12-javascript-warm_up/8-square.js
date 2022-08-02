2db5b66599fdd2282731197638542c8e2a25b749
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-02 00:58:51 -0500

    Correction of identation

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@
 const myArgv = process.argv[2];
 
 if (isNaN(myArgv)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   const convertInteger = Math.floor(myArgv);
- 
+
   for (let i = 0; i < convertInteger; i++) {
     console.log('X'.repeat(convertInteger));
   }

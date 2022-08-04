cdcadda8f25b035148210f02cb644b2896b0b70f
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-03 23:29:57 -0500

    Adding task advance

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const fs = require('fs');
+const dataA = fs.readFileSync(process.argv[2], { encoding: 'utf8', flag: 'r' });
+const dataB = fs.readFileSync(process.argv[3], { encoding: 'utf8', flag: 'r' });
+const dataC = dataA + '\n' + dataB;
+fs.writeFileSync(process.argv[4], dataC, { encoding: 'utf8', flag: 'w' });

47cff56730be5f7e0369c6e77df4c6ca71740bf8
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-03 23:35:55 -0500

    Update task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+// concat
+
 const fs = require('fs');
 const dataA = fs.readFileSync(process.argv[2], { encoding: 'utf8', flag: 'r' });
 const dataB = fs.readFileSync(process.argv[3], { encoding: 'utf8', flag: 'r' });
-const dataC = dataA + '\n' + dataB + '\n';
+const dataC = dataA.concat(dataB);
 fs.writeFileSync(process.argv[4], dataC, { encoding: 'utf8', flag: 'w' });

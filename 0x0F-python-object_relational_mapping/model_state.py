41673bd320b479b8baa0ca17472c7eeb5c7bb878
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-05 23:28:15 -0700

    feat: Adding first statel model for create table in the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Creation of state model == table state"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Creacion de state model"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)

535b8078c7fe79231261853529b3409a9a48d6aa
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-05 22:23:04 -0700

    fix: change from like to like binary so that it differentiates if it is uppercase or lowercase

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name LIKE 'N%' \
+                 WHERE name LIKE BINARY 'N%' \
                  ORDER BY states.id")
 
     rows = cur.fetchall()

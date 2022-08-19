f584bae430c9e818bfb17cf58792a5c454accf3c
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-19 03:25:53 +0000

    script that makes a request to 0.0.0.0 5000/catch_me that causes the server to respond with a message containing You got me in the body of the response.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond 
 # with a message containing You got me!, in the body of the response.
-curl -s 0.0.0.0:5000/catch_me -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -X PUT -L -H "Origin: HolbertonSchool" -s 0.0.0.0:5000/catch_me -d "user_id=98"

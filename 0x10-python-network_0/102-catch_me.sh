316a156e7f51cf8c7bcb2b159b1a1a3ec71bce70
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-19 03:33:41 +0000

    fix: correction of sort

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond 
-# with a message containing You got me!, in the body of the response.
-curl -X PUT -L -H "Origin: HolbertonSchool" -s 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me

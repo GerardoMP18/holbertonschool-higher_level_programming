624a0be8deb7c8647d76029351f498fcdcf11400
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-13 22:29:09 -0500

    [fix] Adding new version of get result of task completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,6 +4,7 @@
 const endPoint = process.argv[2];
 const axios = require('axios').default;
 
+/*
 axios.get(endPoint).then(
   (response) => {
     const myDict = {};
@@ -17,6 +18,19 @@ axios.get(endPoint).then(
       }
     });
     console.log(myDict);
+  }).catch(
+  (error) => {
+    console.log(error);
+  });
+ */
+
+// filter amazing
+axios.get(endPoint).then(
+  (response) => {
+    const search = response.data.forEach(element => element
+      .filter(done => done.completed === true)
+    );
+    console.log(search);
   }).catch(
   (error) => {
     console.log(error);

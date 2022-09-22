fba04ae1270eb36bae24a051d5803e3beda7926b
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-21 23:10:40 -0500

    [feat] Adding script that updates the text color of the <header> element of color red with DOMContentLoad

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+// script that updates the text color of the <header> element to red (#FF0000):
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('header').style.color = '#FF0000';
+});

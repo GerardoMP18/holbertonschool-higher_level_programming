87496651407a6cd32737240e13eefc55c1d2412c
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-22 01:04:31 -0500

    [feat] Adding script that adds, removes and clears LI elements from a list when the user clicks

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+// script that adds, removes and clears LI elements from a list when the user clicks:
+document.addEventListener('DOMContentLoaded', () => {
+  $('DIV#add_item').click(() => {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(() => {
+    $('UL.my_list li').last().remove();
+  });
+  $('DIV#clear_list').click(() => {
+    $('UL.my_list').empty();
+  });
+});
